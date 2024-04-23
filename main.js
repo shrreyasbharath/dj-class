9fd34a24b4d0fb6339e567d0d422922374b4ab17
shrreyasbharath <85285429+shrreyasbharath@users.noreply.github.com>
2024-04-23 14:59:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -60,12 +60,12 @@ function gotPoses(results)
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist = " + scoreLeftWrist);
 
-        leftWristX = results[0].pose.leftWrist.X;
-        leftWristY = results[0].pose.leftWrist.Y;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY = " + leftWristY);
 
-        rightWristX = results[0].pose.rightWrist.X;
-        rightWristY = results[0].pose.rightWrist.Y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
     }
 }
